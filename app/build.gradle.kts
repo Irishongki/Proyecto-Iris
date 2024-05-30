f1a4ffcc87085c5239e00db36e969717fd1f48fd
Irene Cañadas Pastor <48756218+Irishongki@users.noreply.github.com>
2024-05-30 20:05:02 +0200

    Actualizaciones de librerias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,22 +50,22 @@ dependencies {
 
     implementation("com.google.android.gms:play-services-maps:18.2.0")
     //Mapas y Fragments
-    implementation("androidx.fragment:fragment-ktx:1.6.2")
+    implementation("androidx.fragment:fragment-ktx:1.7.1")
     implementation ("org.osmdroid:osmdroid-android:6.1.10")
 
     //WebView
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.2.0-alpha01")
     //Auth FireBase
-    implementation("com.google.firebase:firebase-auth:22.3.1")
-    implementation("com.google.android.gms:play-services-auth-base:18.0.12")
-    implementation("com.google.android.gms:play-services-auth:21.1.0")
+    implementation("com.google.firebase:firebase-auth:23.0.0")
+    implementation("com.google.android.gms:play-services-auth-base:18.0.13")
+    implementation("com.google.android.gms:play-services-auth:21.2.0")
 
-    implementation("androidx.core:core-ktx:1.13.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.google.firebase:firebase-database:20.3.1")
-    implementation("com.google.firebase:firebase-storage:20.3.0")
+    implementation("com.google.firebase:firebase-database:21.0.0")
+    implementation("com.google.firebase:firebase-storage:21.0.0")
     implementation("androidx.gridlayout:gridlayout:1.0.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
